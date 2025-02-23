14aae68f8b77c5c3d480b042904aa44d4d7e88ac
elshreef <muhammedelshreef@outlook.com>
2025-02-23 04:12:50 +0200

    .

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,83 +8,79 @@ const links = ref([
   { name: "contact", url: "#contact" },
 ]);
 
-const currentHash = ref<string>(window.location.hash);
+// const currentHash = ref<string>(window.location.hash);
 
-const scrollToSection = (url: string) => {
-  const section = document.querySelector(url);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-};
+// const scrollToSection = (url: string) => {
+//   const section = document.querySelector(url);
+//   if (section) {
+//     section.scrollIntoView({ behavior: "smooth" });
+//   }
+// };
 
-const updateCurrentHash = () => {
-  const sections = links.value.map((link) => document.querySelector(link.url));
-  const scrollPosition = window.scrollY + window.innerHeight / 2;
+// const updateCurrentHash = () => {
+//   const sections = links.value.map((link) => document.querySelector(link.url));
+//   const scrollPosition = window.scrollY + window.innerHeight / 2;
 
-  for (const section of sections) {
-    if (section) {
-      const sectionElement = section as HTMLElement;
-      const sectionTop = sectionElement.offsetTop;
-      const sectionBottom = sectionTop + sectionElement.offsetHeight;
+//   for (const section of sections) {
+//     if (section) {
+//       const sectionElement = section as HTMLElement;
+//       const sectionTop = sectionElement.offsetTop;
+//       const sectionBottom = sectionTop + sectionElement.offsetHeight;
 
-      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-        currentHash.value = `#${sectionElement.id}`;
-        break;
-      }
-    }
-  }
-};
+//       if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+//         currentHash.value = `#${sectionElement.id}`;
+//         break;
+//       }
+//     }
+//   }
+// };
 
-let observer: IntersectionObserver | null = null;
+// let observer: IntersectionObserver | null = null;
 
-onMounted(() => {
-  window.addEventListener("scroll", updateCurrentHash);
+// onMounted(() => {
+//   window.addEventListener("scroll", updateCurrentHash);
 
-  observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          currentHash.value = `#${entry.target.id}`;
-        }
-      });
-    },
-    {
-      rootMargin: "0px",
-      threshold: 0.5,
-    }
-  );
+//   observer = new IntersectionObserver(
+//     (entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           currentHash.value = `#${entry.target.id}`;
+//         }
+//       });
+//     },
+//     {
+//       rootMargin: "0px",
+//       threshold: 0.5,
+//     }
+//   );
 
-  links.value.forEach((link) => {
-    const section = document.querySelector(link.url);
-    if (section) {
-      observer?.observe(section);
-    }
-  });
-});
+//   links.value.forEach((link) => {
+//     const section = document.querySelector(link.url);
+//     if (section) {
+//       observer?.observe(section);
+//     }
+//   });
+// });
 
-onUnmounted(() => {
-  window.removeEventListener("scroll", updateCurrentHash);
-  if (observer) {
-    observer.disconnect();
-  }
-});
+// onUnmounted(() => {
+//   window.removeEventListener("scroll", updateCurrentHash);
+//   if (observer) {
+//     observer.disconnect();
+//   }
+// });
 </script>
 
 <template>
   <div class="fixed transform -translate-x-1/2 left-1/2 z-50 top-5">
     <nav>
       <div
-        class="bg-[#2B2A2A]/10 backdrop-blur-2xl border-[1px] border-paragraph rounded-3xl flex items-center justify-center py-3 px-8 transition-all duration-300 ease-in-out"
+        class="backdrop-blur-2xl border-[1px] border-paragraph rounded-3xl flex items-center justify-center py-3 px-8 transition-all duration-300 ease-in-out"
       >
         <ul class="flex items-center lg:gap-6 gap-4 text-paragraph">
           <li v-for="(link, index) in links" :key="link.name">
             <a
               class="text-md hover:text-azure hover:cursor-pointer capitalize transition-all duration-300 ease-in-out"
-              :class="{
-                'text-azure': currentHash === link.url,
-              }"
               :href="link.url"
-              @click.prevent="scrollToSection(link.url)"
             >
               {{ link.name }}
             </a>
